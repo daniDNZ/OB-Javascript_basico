640036d6ff31d196b542a79b8756d2ceb33ac20e
danisanz <danielsanzelguer@gmail.com>
2022-06-05 11:49:59 +0200

    Exercise 2 finished

---
+++ 2.Syntaxis/index.js
@@ -0,0 +1,14 @@
+const ar = [
+  "daniel",
+  30,
+  true,
+  new Date("11-1-1991"),
+  {
+    title: "El nombre del viento",
+    author: "Patrick Rothfuss",
+    date: "2007",
+    url: "https://www.amazon.es/El-nombre-del-viento-EXITOS/dp/8401352347/ref=sr_1_2?__mk_es_ES=ÅMÅŽÕÑ&crid=8ODJLLILE9JS&keywords=el+nombre+del+viento&qid=1654420672&sprefix=el+nombre+del+viento%2Caps%2C92&sr=8-2",
+  },
+];
+
+console.log(ar);
